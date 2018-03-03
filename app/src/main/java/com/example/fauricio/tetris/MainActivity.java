429aa286c32679ab28780609f554f322061974c0
fauricioNavarro <fauriciocr@gmail.com>
2018-03-02 22:17:40 -0600

    Creación de tablero

--- app/src/main/java/com/example/fauricio/tetris/MainActivity.java
+++ app/src/main/java/com/example/fauricio/tetris/MainActivity.java
@@ -1,40 +1,86 @@
 package com.example.fauricio.tetris;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private GridLayout gridLayout;
+    private GridView gridView;
+
+    public static Integer[] imageIDs = new Integer[264];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gridLayout = findViewById(R.id.tablero_gui);
+        gridView = findViewById(R.id.tablero_gui);
+        int base= 12;
         for(int i=0;i<22;i++){
             for(int j=0;j<12;j++){
-                ImageView imageView = new ImageView(this);
-                if((i==0 || i==21)&&(j==0||j==11)){
-                    imageView.setImageResource(R.drawable.gray);
-                    gridLayout.addView(imageView,i,j);
+                if(j==0||j==11){
+                    imageIDs[(base*i)+j] = R.drawable.gray;
+                }else if(i==0 || i==21){
+                    imageIDs[(base*i)+j] = R.drawable.gray;
                 }else{
-                    imageView.setImageResource(R.drawable.black);
-                    gridLayout.addView(imageView,i,j);
-                    GridLayout.LayoutParams param =new GridLayout.LayoutParams();
-                    param.height = GridLayout.LayoutParams.WRAP_CONTENT;
-                    param.width = GridLayout.LayoutParams.WRAP_CONTENT;
-                    param.rightMargin = 5;
-                    param.topMargin = 5;
-                    param.setGravity(Gravity.CENTER);
-                    param.columnSpec = GridLayout.spec(j);
-                    param.rowSpec = GridLayout.spec(i);
-                    gridLayout.setLayoutParams (param);
+                    imageIDs[(base*i)+j] = R.drawable.black;
                 }
-
             }
+        }
+        gridView.setAdapter(new ImageAdapter(this));
+    }
+
+    
+    public class ImageAdapter extends BaseAdapter
+    {
+
+        private Context context;
+
+        public ImageAdapter(Context c)
+        {
+            context = c;
+        }
+
+        //---returns the number of images---
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        //---returns the ID of an item---
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        //---returns an ImageView view---
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setLayoutParams(new GridView.LayoutParams(45, 45));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(1, 1, 1, 1);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(imageIDs[position]);
+            return imageView;
         }
     }
 }
